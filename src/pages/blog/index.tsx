8dd284f3b80a1455761ca743c2c305a8fcec0e26
TaroXin <TaroXin@users.noreply.github.com>
2023-02-08 09:55:18 +0000

    deploy: f3352539971b53a59c14e7920aab0f0fe8fc893b

--- src/pages/blog/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-export default function () {
-  const [count] = useState(0)
-  const [message] = useState('this is test message')
-
-  return <div> Hello Blog: {count} - {message} </div>
-}
